93a83a29106cd257d4ca59316b7689c3512cc08a
zechfox <zechfox@gmail.com>
2016-11-04 04:42:07 -0400

    Chapter 7, Exercise Conceptual

---
+++ ch7/conceptual.py
@@ -0,0 +1,30 @@
+# Chapter 7, Exercises Conceptual
+
+#Exercise 1
+print('Exercise 1 ')
+print('(a) for x=<epsilon, a1=beta0, b1=beta1, c1=beta2, d1=beta3.')
+print('(b) for x>epsilon, expand beta4(x-epsilon)^3, get a2=beta0-beta4*epsilon^3, b2=beta1+3beta4*epsilon^2, c2=beta2-3beta4*epsilon, d2=beta3+beta4.')
+print('(c) put answer (b) into f2(epsilon), will get f1(epsilon)=f2(epsilon)')
+print('(d) derivate f1(x) and f2(x), then get the equation.')
+print('(e) same as (d)')
+#Exercise 2
+print('Exercise 2 ')
+print('(a) penalt is very large, the curve shoud be very smooth, nearly a straight line.')
+print('(b) same as (a).')
+print('(c) same as (a).')
+print('(d) same as (a).')
+print('(e) penalt is 0, penalty has no effect, curve will be very jumpy and reflect real training set.')
+
+#Exercise 3
+print('Exercise 3, ignore.')
+
+#Exercise 4
+print('Exercise 4, ignore. ')
+
+
+#Exercise 5
+print('Exercise 5 ')
+print('(a) no, ')
+print('(b) no ')
+print('(c) yes, g1 and g2 are same')
+
